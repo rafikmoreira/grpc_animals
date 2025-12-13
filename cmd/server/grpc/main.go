ff4304ef217fc9126720d9c4076463484d20d186
Rafik Moreira <rafikmoreiradias@outlook.com>
2025-12-13 16:19:59 -0300

    update server

--- cmd/server/grpc/main.go
+++ cmd/server/grpc/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
-	"log"
+	"fmt"
+	"os"
+
 	"net"
 
 	animalsv1 "github.com/rafikmoreira/animals/pkg/proto/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	"github.com/rs/zerolog/log"
 )
 
 type Server struct {
@@ -16,32 +20,34 @@ type Server struct {
 }
 
 func (s *Server) GetAnimal(ctx context.Context, req *animalsv1.GetAnimalRequest) (*animalsv1.GetAnimalResponse, error) {
-	log.Print(req)
+	log.Info().Msg("Listando animais!")
 	return nil, status.Error(codes.Unimplemented, "method GetAnimal not implemented")
 }
 func (s *Server) CreateAnimal(ctx context.Context, req *animalsv1.CreateAnimalRequest) (*animalsv1.CreateAnimalResponse, error) {
-	log.Print(req)
+	log.Info().Str("animal_name", req.GetName()).Str("animal_type", req.GetAnimalType().String()).Msg("Dados do animal")
 	return nil, status.Error(codes.Unimplemented, "method CreateAnimal not implemented")
 }
 
 func main() {
+
 	grpcServer := grpc.NewServer()
 
 	server := new(Server)
 
 	animalsv1.RegisterAnimalsServiceServer(grpcServer, server)
 
-	port := ":9091"
+	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
 
 	listener, err := net.Listen("tcp", port)
 
 	if err != nil {
-		panic(err.Error())
+		log.Error().Err(err)
 	}
 
+	log.Info().Str("port", port).Msg("Inicializando servidor")
 	err = grpcServer.Serve(listener)
 
 	if err != nil {
-		panic(err.Error())
+		log.Error().Err(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/rafikmoreira/animals
 go 1.25.5
 
 require (
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.34.0 // indirect
 	golang.org/x/net v0.46.1-0.20251013234738-63d1a5100f82 // indirect
-	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251022142026-3a174f9686a8 // indirect
 	google.golang.org/grpc v1.77.0 // indirect
